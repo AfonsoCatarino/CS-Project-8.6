43c45c594133315d4d5fe599e0a2212dc28bbf29
AfonsoCatarino <147815159+AfonsoCatarino@users.noreply.github.com>
2023-12-14 23:17:35 +0100

    Update cspfinaltest.py

--- cspfinaltest.py
+++ cspfinaltest.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+import base64
+import requests
+import xml.etree.ElementTree as ET
 
 class Footprint:
     def __init__(self):
